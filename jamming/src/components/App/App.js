6f031d3ef53f88e65353ebdb352bc19016e675a5
laicuRoot <jdblancoes@gmail.com>
2020-07-09 22:45:08 +0100

    added constructor in App.js

--- jamming/src/components/App/App.js
+++ jamming/src/components/App/App.js
@@ -4,19 +4,24 @@ import {SearchBar} from '../SearchBar/SearchBar';
 import {SearchResults} from '../SearchResults/SearchResults';
 import {PlayList} from '../PlayList/PlayList';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-        <SearchBar />
-        <div className="App-playlist">
-          <SearchResults />
-          <PlayList />
+class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults />
+            <PlayList />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
