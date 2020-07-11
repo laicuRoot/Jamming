b50a970f820d0cb25b3ba64a10060c952e7d32a8
laicuRoot <jdblancoes@gmail.com>
2020-07-11 10:57:22 +0100

    passed to playlist from app playlist name & track

--- jamming/src/components/App/App.js
+++ jamming/src/components/App/App.js
@@ -28,6 +28,27 @@ class App extends React.Component{
           album: 'album3',
           id: 3
         }
+      ],
+      playListName: 'playList 1',
+      playlistTracks: [
+        {
+          name: 'name1',
+          artist: 'artist1',
+          album: 'album1',
+          id: 1
+        },
+        {
+          name: 'name2',
+          artist: 'artist2',
+          album: 'album2',
+          id: 2
+        },
+        {
+          name: 'name3',
+          artist: 'artist3',
+          album: 'album3',
+          id: 3
+        }
       ]
     };
 
@@ -40,7 +61,7 @@ class App extends React.Component{
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults}/>
-            <PlayList />
+            <PlayList playListName={this.state.playListName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
