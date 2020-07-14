6621507f66b8dc2ef1f3d385cfa997dc3adf3b6c
laicuRoot <jdblancoes@gmail.com>
2020-07-14 22:09:19 +0100

    fetch added to Spotify module

--- jamming/src/util/Spotify.js
+++ jamming/src/util/Spotify.js
@@ -3,7 +3,7 @@ const clientID = '0853a1432982471cb6b88ab9bdef9158';
 const redirectURI = ''
 
 const Spotify = {
-  getAccessToke(){
+  getAccessToken(){
     if(accessToken){
       return accessToken
     }
@@ -22,9 +22,30 @@ const Spotify = {
       const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
       window.location = accessURL;
     }
-
+  },
+  async search(term){
+    const accessToken = Spotify.getAccessToken();
+    const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, 
+    {
+      headers: 
+      {
+        Authorization: `Bearer ${accessToken}`
+      }
+    });
+    const jsonResponse = await response.json();
+    if (!jsonResponse.tracks){
+      return [];
+    }
+    return jsonResponse.tracks.items.map((track)=>{
+      return {
+        id: track.id,
+        name: track.name,
+        artist: track.artist[0].name,
+        album: track.album[0].name,
+        uri: track.uri
+      }
+    })
   }
-  
 };
 
 export default Spotify;
