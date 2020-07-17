3cd718b4b97cbac36322ec163d1c5b2763f93985
laicuRoot <jdblancoes@gmail.com>
2020-07-17 11:38:34 +0100

    fixed tracklist

--- jamming/src/components/PlayList/PlayList.js
+++ jamming/src/components/PlayList/PlayList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Tracklist} from '../TrackList/Tracklist';
+import {Tracklist} from '../TrackList/TrackList';
 import './PlayList.css';
 
 
--- jamming/src/components/SearchResults/SearchResults.js
+++ jamming/src/components/SearchResults/SearchResults.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './SearchResults.css';
-import {Tracklist} from '../TrackList/Tracklist';
+import {Tracklist} from '../TrackList/TrackList';
 
 export class SearchResults extends React.Component{
   render(){
