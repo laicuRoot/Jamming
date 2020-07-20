372e56ec7f929d09cae744deb08fe40b159d9d65
laicuRoot <jdblancoes@gmail.com>
2020-07-20 22:23:28 +0100

    removed accessToken logs

--- jamming/src/util/Spotify.js
+++ jamming/src/util/Spotify.js
@@ -6,7 +6,6 @@ const redirectURI = 'http://localhost:3000/'
 export const Spotify = {
   getAccessToken(){
     if(accessToken){
-      console.log(accessToken)
       return accessToken
     }
     //check for access token match
@@ -19,7 +18,6 @@ export const Spotify = {
       //This clears the parameters, allowing us to grab new access token when it expires
       window.setTimeout(() => accessToken='', expiresIn*1000);
       window.history.pushState('Access token', null, '/');
-      console.log(accessToken)
       return accessToken;
     } else {
       const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
@@ -28,7 +26,6 @@ export const Spotify = {
   },
   async search(term){
     const accessToken = Spotify.getAccessToken();
-    console.log(accessToken)
     const response = await fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, 
     {
       headers: 
@@ -53,11 +50,11 @@ export const Spotify = {
   async getUserId() {
     const accessToken = Spotify.getAccessToken();
     if (userid) {
+      console.log(userid)
       return fetch(`https://api.spotify.com/v1/users/${userid}`,
         {
           headers: { Authorization: `Bearer ${accessToken}` },
           mode:'no-cors'
-
         })
     } else {
       const response = await fetch('https://api.spotify.com/v1/me', {
@@ -91,7 +88,6 @@ export const Spotify = {
     // );
     // const jsonResponse = await response.json();
     // userID = jsonResponse.id;
-
 
     const response_1 = await fetch(`https://api.spotify.com/v1/users/${userid}/playlists`,
       {
